4ba44c07db1e6db93e8ec9372eefc32ae500bf7f
Kevin Jobson <jobsonk2877@gmail.com>
2019-10-02 08:59:12 -0500

    Classes built, user interface skeletoned out

---
+++ LemonadeStand/LemonadeStand/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Game
+    {
+        // member variables
+
+
+
+        // constructor
+
+        public void RunGame()
+        {
+            UserInterface.DisplayRules();
+            UserInterface.DisplayInventory();
+            UserInterface.DisplayWeather();
+        }
+
+        //member methods
+    }
+}
---
+++ LemonadeStand/LemonadeStand/UserInterface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    static class UserInterface
+    {
+        // all methods public static void
+
+
+        // member variables
+
+
+
+
+
+        //member methods
+
+        public static void DisplayRules()
+        {
+
+            Console.WriteLine(" Filler Display Rules ");
+            Console.ReadLine();
+
+        }
+
+        public static void DisplayInventory(Inventory inventory)
+        {
+            Console.WriteLine("Filler Display Inventory");
+
+
+        }
+
+        public static void DisplayWeather(Weather weather)
+        {
+            Console.WriteLine("Filler Display Weather");
+
+        }
+
+        
+
+
+
+
+    }
+}
