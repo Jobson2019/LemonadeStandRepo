8f29b562a3cf48ecf07679590cb2f99ab5be9ffa
Kevin Jobson <jobsonk2877@gmail.com>
2019-10-03 09:18:42 -0500

    generate customer name coded

--- LemonadeStand/LemonadeStand/Customer.cs
+++ LemonadeStand/LemonadeStand/Customer.cs
@@ -11,17 +11,45 @@ namespace LemonadeStand
         static Random random = new Random(DateTime.Now.Millisecond);
         public double tempMultiplier;
         public double weatherMultiplier;
+        public List<string> customerNames;
+        public string name;
 
+
+
+
+        public Customer()
+        {
+            customerNames = new List<string>()
+            {
+            "Randy", "Big Dirty Dave", "Jack", "Beta-Male Eric", "Marky Mark", "Ice T", "Racist Dan", "Tom Sellick's Moustache", ""
+            };
+        }
+
+
+
+
+        //Find out how to pull one of these names
+        public void ChooseName()
+        {
+            int genName = random.Next(0, 11);
+            name = customerNames[genName];
+        }
+
+        
+        
+        
+        
+        
         // create demand method
         public bool BuyChance(int weather, int temp)
         {
-        
+        // weather switch all except 0 to else if statments
 
             if (weather == 0)
             { 
                 weatherMultiplier = 0.4;
             }
-            if (weather == 1)
+            else if (weather == 1)
             {
                 weatherMultiplier = 0.6;
             }
@@ -49,6 +77,9 @@ namespace LemonadeStand
             {
                 weatherMultiplier = 1.4;
             }
+
+
+
             if (temp == 0)
             {
                 tempMultiplier = 0.4;
@@ -82,7 +113,7 @@ namespace LemonadeStand
                 tempMultiplier = 1.4;
             }
 
-            double chanceToBuy = random.Next(1, 100);
+            int chanceToBuy = random.Next(1, 100);
             double buyMod = (tempMultiplier * weatherMultiplier * chanceToBuy);
             if (buyMod >= 50)
             
